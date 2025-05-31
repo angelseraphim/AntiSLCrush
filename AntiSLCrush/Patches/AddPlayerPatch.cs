bf782e99383988a4088fae6473bd775321ab9486
Angel seraphim <server@praniksl.ru>
2025-06-01 00:18:27 +0500

    LabApi Player.AddPlayer method fix

---
+++ AntiSLCrush/Patches/AddPlayerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using LabApi.Features.Console;
+using LabApi.Features.Wrappers;
+
+namespace AntiSLCrush.Patches
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.AddPlayer))]
+    internal class AddPlayerPatch
+    {
+        private static void Prefix(ReferenceHub referenceHub)
+        {
+            if (Player.Dictionary.ContainsKey(referenceHub))
+            {
+                Logger.Warn($"{referenceHub.nicknameSync.MyNick} already in Dictionary");
+                Player.Dictionary.Remove(referenceHub);
+            }
+        }
+    }
+}
